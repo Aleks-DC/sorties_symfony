fbc37ec19e25bb29147c1f9f79826f03943d3712
Corentin Rouff <Rouff.corentin@hotmail.fr>
2024-08-08 12:05:39 +0200

    chore: Update CSS and add navigation bar to base template

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CampusRepository;
+use App\Repository\ParticipantRepository;
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    #[Route('/accueil', name: 'app_accueil')]
+    public function index(ParticipantRepository $participantRepository, SortieRepository $sortieRepository, CampusRepository $campusRepository): Response
+    {
+        $date = new DateTime();
+        $frDate = $date->format('d/m/Y');
+        $participants = $participantRepository->findAll()[2]->getNom();
+        $sorties = $sortieRepository->findAll();
+        $campus = $campusRepository->findBy([]);
+
+        return $this->render('global/accueil.html.twig', [
+            'controller_name' => 'AccueilController',
+            'date' => $frDate,
+            'participants' => $participants,
+            'sorties' => $sorties,
+            'campus' => $campus,
+        ]);
+    }
+
+    #[Route('/search', name: 'app_search')]
+    public function search(ParticipantRepository $participantRepository, SortieRepository $sortieRepository): Response
+    {
+        $date = new DateTime();
+        $frDate = $date->format('d/m/Y');
+        $participants = $participantRepository->findAll()[2]->getNom();
+        $sortie = $sortieRepository->findAll();
+
+        return $this->render('global/search.html.twig', [
+            'controller_name' => 'AccueilController',
+            'date' => $frDate,
+            'participants' => $participants,
+            'sortie' => $sortie,
+        ]);
+    }
+}
