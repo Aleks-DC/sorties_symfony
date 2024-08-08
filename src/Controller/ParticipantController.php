8b0a9aae8d636ad5a1251b816ad659c702370dc5
Abdallah B. <m.bougjdi@hotmail.fr>
2024-08-08 09:42:35 +0200

    Affichage pages modifier/afficher profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/participant', name: 'participant_')]
@@ -28,20 +29,39 @@ class ParticipantController extends AbstractController{
         ]);
     }
 
-    #[Route('/modifierProfil', name: 'modifierProfil', methods: ['POST'])]
-    public function modifierProfil(Request $request): Response
+    #[Route('/modifierProfil/{id}', name: 'modifierProfil')]
+    public function modifierProfil(Request $request, EntityManagerInterface $entityManager, int $id): Response
     {
-        $participantForm = $this->createForm(ParticipantType::class);
+        $participant = $entityManager->getRepository(Participant::class)->find($id);
+        if (!$participant) {
+            throw $this->createNotFoundException('Le participant n\'existe pas');
+        }
 
+        $participantForm = $this->createForm(ParticipantType::class, $participant);
         $participantForm-> handleRequest($request);
 
         if($participantForm->isSubmitted() && $participantForm->isValid()){
-            //faire qqch avec les données
-            //dump($participant)
+            $motDePasseActuel = $participantForm->get('motDePasseActuel')->getData();
+            $nouveauMotDePasse = $participantForm->get('nouveauMotDePasse')->getData();
+            $confirmationNouveauMotDePasse = $participantForm->get('confirmationNouveauMotDePasse')->getData();
 
-            return $this->redirectToRoute('modifierProfil');
+            if ($motDePasseActuel !== $participant->getMotDePasse()) {
+                $this->addFlash('error', 'Le mot de passe actuel est incorrect.');
+            } elseif (empty($nouveauMotDePasse) || $nouveauMotDePasse !== $confirmationNouveauMotDePasse) {
+                $this->addFlash('error', 'Le nouveau mot de passe et la confirmation ne correspondent pas.');
+            } else {
+                // Remplacez le mot de passe par le nouveau mot de passe
+                $participant->setMotDePasse($nouveauMotDePasse);
+                $this->addFlash('success', 'Mot de passe mis à jour avec succès.');
+
+                // Persistez les modifications
+                $entityManager->persist($participant);
+                $entityManager->flush();
+
+                // Redirection après mise à jour réussie
+                return $this->redirectToRoute('participant_profil', ['id' => $id]);
+            }
         }
-
         return $this->render('participant/modifier.html.twig', [
             'participantForm' => $participantForm->createView(),
         ]);
