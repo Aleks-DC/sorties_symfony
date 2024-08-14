6f509f41118b02adfd0b7276de9294ea8e38caeb
Aleks_DC <165147128+Aleks-DC@users.noreply.github.com>
2024-08-14 15:36:27 +0200

    modif AccueilController + accueil.html pour affichage dynamique pseudo

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,28 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Repository\CampusRepository;
 use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use DateTime;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(ParticipantRepository $participantRepository, SortieRepository $sortieRepository, CampusRepository $campusRepository): Response
+    public function index(ParticipantRepository $participantRepository, SortieRepository $sortieRepository, CampusRepository $campusRepository, Security $security): Response
     {
         $date = new DateTime();
         $frDate = $date->format('d/m/Y');
-        $participants = $participantRepository->findAll()[2]->getNom();
+
+        // Obtenir l'utilisateur connecté
+        $user = $security->getUser();
+        $username = $user instanceof Participant ? $user->getPseudo() : 'Utilisateur non connecté';
+
+        $participants = $participantRepository->findAll();
         $sorties = $sortieRepository->findAll();
         $campus = $campusRepository->findBy([]);
 
         return $this->render('global/accueil.html.twig', [
             'controller_name' => 'AccueilController',
             'date' => $frDate,
+            'username' => $username,
             'participants' => $participants,
             'sorties' => $sorties,
             'campus' => $campus,
